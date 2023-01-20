7a84c08bfd1dd597a50dea9f17b266bf06191642
Ventru67 <ventru67@gmail.com>
2023-01-20 22:19:33 +0300

    First task done

---
+++ Ex001/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Write("Введите номер дня недели(1-7): ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if (number <= 5)
+{
+    Console.WriteLine("Будний день");
+}
+else 
+{
+    Console.WriteLine("Выходной день");
+}
