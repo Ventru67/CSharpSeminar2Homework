dd12294206a92b3fe60d8557b9e9a44bfd42afd9
Ventru67 <ventru67@gmail.com>
2023-01-20 23:31:10 +0300

    Third task done

---
+++ Ex003/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.Write("Задайте номер четверти(1-4): ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if (number == 1)
+{
+    Console.WriteLine("Диапазон координат [+infinity; +infinity]");
+}
+if (number == 2)
+{
+    Console.WriteLine("Диапазон координат [-infinity; +infinity]");
+}
+if (number == 3)
+{
+    Console.WriteLine("Диапазон координат [-infinity; -infinity]");
+}
+if (number == 4)
+{
+    Console.WriteLine("Диапазон координат [+infinity; -infinity]");
+}
